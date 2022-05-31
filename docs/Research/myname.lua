9252dcbb8a9d82091a7947a016ed3ab493c29467
blostein <blostein@umich.edu>
2022-05-31 15:00:52 -0400

    cv updates

--- docs/Research/myname.lua
+++ docs/Research/myname.lua
@@ -4,10 +4,10 @@ local highlight_author_filter = {
     for k,_ in ipairs(el.content) do
       if el.content[k].t == "Str" and el.content[k].text == "Blostein,"
       and el.content[k+1].t == "Space"
-      and el.content[k+2].t == "Str" and el.content[k+2].text:find("^F.") then
-          local _,e = el.content[k+2].text:find("^F.")
+      and el.content[k+2].t == "Str" and el.content[k+2].text:find("^F.*") then
+          local _,e = el.content[k+2].text:find("^F.*")
           local rest = el.content[k+2].text:sub(e+1) 
-          el.content[k] = pandoc.Strong { pandoc.Str("Blostein, F.") }
+          el.content[k] = pandoc.Strong { pandoc.Str("Blostein, F,") }
           el.content[k+1] = pandoc.Str(rest)
           table.remove(el.content, k+2) 
       end
